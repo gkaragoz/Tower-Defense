582908039a9d84be8d2efb99ceabf112667920bb
Ib Quezada <ib@ibquezada.com>
2016-08-07 22:26:35 -0700

    Add Gizmo for path

---
+++ Assets/Scripts/PathGizmo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathGizmo : MonoBehaviour {
+    private int _childCount;
+
+    void Start()
+    {
+        _childCount = transform.childCount;
+    }
+
+    void OnDrawGizmos()
+    {
+        int i = 0;
+        Transform prevChild = null;
+
+        foreach(Transform child in transform)
+        {
+            if (i > 0 && i < transform.childCount && prevChild != null)
+            {
+                Gizmos.DrawLine(prevChild.position, child.position);
+                Debug.Log("Drawing");
+            }
+            i++;
+            prevChild = child;
+        }
+    }
+}
