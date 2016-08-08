1366b072445d97824d2ce39735df1ab9e4e76766
Gökhan Karagöz <annotto3@gmail.com>
2016-08-09 01:26:22 +0300

    Merge ibito, enemy health

---
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealthBar : MonoBehaviour {
+    float _health;
+    float _fullHealth;
+
+    RectTransform _rectTransform;
+    EnemyManager _enemyManager;
+
+    void Start()
+    {
+        _enemyManager = transform.parent.gameObject.GetComponent<EnemyManager>();
+        _rectTransform = GetComponent<RectTransform>();
+        _fullHealth = _enemyManager.Health;
+        Debug.Log("ENEMYHEALTHBAR.CS " + _enemyManager.Health);
+    }
+
+	void Update () {
+        _health = _enemyManager.Health;
+        if (_health > 0)
+        {
+            float healthScale = ((_health * 100) / _fullHealth) / 100f;
+            Debug.Log(_health + " * 100 ) / " + _fullHealth + " / 100");
+            _rectTransform.localScale = new Vector3(healthScale, 1, 1);
+        }
+
+    }
+}
---
+++ Assets/Scripts/FixHealthBarRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixHealthBarRotation : MonoBehaviour {
+    public float verticalOffset = 0.35f;
+    Quaternion rotation;
+    RectTransform _rectTransform;
+
+    void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        rotation = _rectTransform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        _rectTransform.rotation = rotation;
+        transform.position = new Vector3(transform.parent.position.x, 
+                                        transform.parent.position.y + verticalOffset, 
+                                        transform.parent.position.z) ;
+    }
+}
